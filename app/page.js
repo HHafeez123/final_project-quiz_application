97050c278fa523c17b9dd95de014a03211fdf61f
HHafeez123 <144468077+HHafeez123@users.noreply.github.com>
2023-11-05 17:08:34 -0700

    Test

--- app/page.js
+++ app/page.js
@@ -2,6 +2,12 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+      <h1 style={{ fontSize: '2em', marginLeft: '250px' , marginTop: '100px'}} >CPRG 306: Web Development 2 - Assignments</h1>
+      <StudentInfo style={{fontSize: '1em', marginLeft: '250px'}}/>
+      <Link href="/week2" style={{fontSize: '1em', marginLeft: '250px'}}>Week 2</Link>
+      </div>
+      <div></div>
       
     </main>
   )
