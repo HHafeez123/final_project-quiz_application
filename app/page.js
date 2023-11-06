86bfd09e0ac19f12c4ea57bd7ac6503fe2501d63
HHafeez123 <144468077+HHafeez123@users.noreply.github.com>
2023-11-05 17:20:52 -0700

    Fix error for test

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-      <h1 style={{ fontSize: '2em', marginLeft: '250px' , marginTop: '100px'}} >CPRG 306: Web Development 2 - Assignments</h1>
-      <StudentInfo style={{fontSize: '1em', marginLeft: '250px'}}/>
-      <Link href="/week2" style={{fontSize: '1em', marginLeft: '250px'}}>Week 2</Link>
+      <h1 style={{ fontSize: '2em',  marginTop: '10px'}} >CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="/week2" style={{fontSize: '1em'}}>Week 2</Link>
       </div>
       <div></div>
       
